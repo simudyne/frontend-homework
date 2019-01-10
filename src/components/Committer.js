1f5979cb54841dc46884c9e2223696f5224ee1d0
Juan Lloret <juallom@gmail.com>
2019-01-11 00:13:57 +0100

    Remove styled-components dependency as it wasn't being used extensively

--- src/components/Committer.js
+++ src/components/Committer.js
@@ -1,19 +1,18 @@
 import * as React from 'react'
 import {Image} from 'semantic-ui-react'
-import styled from 'styled-components'
 
-const CommitterText = styled.div`
-  color: #586069;
-  font-size: .8rem;
-`
+const style = {
+  color: '#586069',
+  fontSize: '.8rem'
+}
 
 export const Committer = ({author, committer, commit}) => {
 
   const getAvatar = (prop = author) => (
-    <CommitterText>
+    <div style={style}>
       <Image src={prop.avatar_url} avatar />
       <a href={prop.html_url} target="_blank">{prop.login}</a>
-    </CommitterText>
+    </div>
   )
 
   if (author)
@@ -25,8 +24,8 @@ export const Committer = ({author, committer, commit}) => {
   const authored = <a href={`mailto:${commit.author.email}`}>{commit.author.name}</a>
   const committed = <a href={`mailto:${commit.committer.email}`}>{commit.committer.name}</a>
   const avatarUrl = 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png'
-  return <CommitterText>
+  return <div style={style}>
     <Image src={avatarUrl} avatar />
     {authored} authored and {committed} committed
-  </CommitterText>;
+  </div>
 }
